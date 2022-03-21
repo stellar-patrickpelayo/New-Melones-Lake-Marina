eb21183444ee34c60f6cd8136dc56571e75afd4a
stellar-patrickpelayo <patrick.pelayo@stellarims.com>
2022-03-21 13:54:03 -0700

    footer background now appearing correctly

--- footer.php
+++ footer.php
@@ -1,2 +1,15 @@
+<footer class='main-footer'>
+    <div class="upper-footer">
+        <div class='footer-column-wrapper'>
+            <div class='column'>
+                <img class='logo' src=<?php echo get_stylesheet_directory_uri() . '\assets\images\new-melones-logo-light-o3g4hm5wpvlki28ltzysw3vnosfmomt2s69ebvcmkm.png'; ?> />
+            </div>
+            <div class='column'></div>
+            <div class='column'></div>
+            <div class='column'></div>
+        </div>
+    </div>
+    <div class='lower-footer'>
 
-this is the footer
+    </div>
+</footer>
