8c8af7a8d7bc4d78b3344bc045141fd675756c36
stellar-patrickpelayo <patrick.pelayo@stellarims.com>
2022-03-23 15:58:47 -0700

    finished the nav-menu header, still need to make it close when clicking outside the slide out header

--- assets/js/slideOutNavHandler.js
+++ assets/js/slideOutNavHandler.js
@@ -1,12 +1,25 @@
-console.log('works');
 
 jQuery(document).ready(
     ()=>{
-        const navMenuSlideOutTransitionClass = 'nav-menu-transition';
+        const hamburgerMenuSelector = '.hamburger-menu-wrapper';
+        const mobileNavSelector = '.mobile-nav-bar';
+        const liSelector = `${mobileNavSelector} li`;
+
         function toggleSlideOutNavMenu (){
-            jQuery('.mobile-nav-bar').toggleClass(navMenuSlideOutTransitionClass);
+            const navMenuSlideOutTransitionClass = 'nav-menu-transition';
+            const disableOverflowY = 'disable-overflow-y';
+
+            jQuery(mobileNavSelector).toggleClass(navMenuSlideOutTransitionClass);
+            jQuery('html').toggleClass(disableOverflowY);
         }
 
-        jQuery('.hamburger-menu-wrapper').click(toggleSlideOutNavMenu);
-    }
+        function toggleSlideDownMenu(e){
+            const subMenuElem = jQuery(e.currentTarget).find('.sub-menu')[0];
+            jQuery(subMenuElem).slideToggle({duration:150});
+            
+        }
+
+        jQuery(hamburgerMenuSelector).click(toggleSlideOutNavMenu);
+        jQuery(liSelector).click(toggleSlideDownMenu);
+    }   
 )
