0421d7032b62bcb8cfd0e26c3c0747bc95b94206
stellar-patrickpelayo <patrick.pelayo@stellarims.com>
2022-03-23 16:49:26 -0700

    got the video in, taking too long to styler will skip for now to continue on more quickly workable parts of site

---
+++ assets/js/videoOnLoadEventHandler.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(() =>{
+    jQuery('.home-hero-video').on('loadstart', function (){
+        console.log('loaded');
+    })
+});
--- assets/php/enqueueScripts.php
+++ assets/php/enqueueScripts.php
@@ -2,6 +2,8 @@
 
 function enqueueScripts(){
     wp_enqueue_script( 'slideOutNavHandler', get_template_directory_uri() . '/assets/js/slideOutNavHandler.js', array('jquery'), false, true );
+
+    wp_enqueue_script( 'videoOnLoadEventHandler', get_template_directory_uri() . '/assets/js/videoOnLoadEventHandler.js', array('jquery'), false, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueueScripts' );
