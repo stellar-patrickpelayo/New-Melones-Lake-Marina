f59e92795e600a8de593a8af9339a953f84f7e27
stellar-patrickpelayo <patrick.pelayo@stellarims.com>
2022-03-24 08:04:24 -0700

    finished daily slip page, was lots of little things to style

--- page-templates/page-daily-slip-rentals.php
+++ page-templates/page-daily-slip-rentals.php
@@ -5,13 +5,20 @@
     <div class='title-header header-background'>
         <div class='title-wrapper'>
             <div class='inner-wrapper'>
-                <h1>TRANSIENT SLIP 10&#34; x 24&#34;</h1>
+                <h1>TRANSIENT SLIP 10 ' x 24 '</h1>
                 <p>Save time by renting a slip for your stay!</p>
             </div>
         </div>
     </div>
         
     <div class='lower-section'>
+        <div class='price-box'>
+            <span class='price-title'>Daily Rental</span>
+            <span class='item-price-wrapper'>
+                <span class='price-symbol'>$</span>
+                <span class='item-price'>38</span>
+            </span>
+        </div>
         <h2>Overnight Slip Rental</h2>
         <p>24 foot long, 10 foot wide slip.</p>
         <hr/>
