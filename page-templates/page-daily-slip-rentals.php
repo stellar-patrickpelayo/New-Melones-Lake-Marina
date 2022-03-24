c4695d0d263b80d2959b41cf706d8bdeefc16ffc
stellar-patrickpelayo <patrick.pelayo@stellarims.com>
2022-03-23 22:53:39 -0700

    added more template pages, daily slip rentals was tricky

---
+++ page-templates/page-daily-slip-rentals.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Daily Slip Rentals Template */ ?>
+
+<?php get_header();?>
+<main class='daily-slip-rentals general-page'>
+    <div class='title-header header-background'>
+        <div class='title-wrapper'>
+            <div class='inner-wrapper'>
+                <h1>TRANSIENT SLIP 10&#34; x 24&#34;</h1>
+                <p>Save time by renting a slip for your stay!</p>
+            </div>
+        </div>
+    </div>
+        
+    <div class='lower-section'>
+        <h2>Overnight Slip Rental</h2>
+        <p>24 foot long, 10 foot wide slip.</p>
+        <hr/>
+    </div>
+</main>
+<?php get_footer();?>
