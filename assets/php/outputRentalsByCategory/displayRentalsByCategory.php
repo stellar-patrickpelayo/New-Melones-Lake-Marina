bdef9f1161f3a5ed6d894f48795e01d01164d8d5
stellar-patrickpelayo <patrick.pelayo@stellarims.com>
2022-03-25 17:01:00 -0700

    trying wp_query to avoid the looping I am doing

--- assets/php/outputRentalsByCategory/displayRentalsByCategory.php
+++ assets/php/outputRentalsByCategory/displayRentalsByCategory.php
@@ -4,14 +4,26 @@ include_once(get_template_directory() . '/assets/php/outputRentalsByCategory/car
 function displayRentalsByCategory(){
     $excludeID = get_the_ID();
     $categories = get_the_category();
-    $category_id = $GLOBALS['RENTALS'] !== $categories[0]->cat_ID ? $categories[0]->cat_ID : $categories[1]->cat_ID;
+    
+
+    if(count($categories) > 1){
+        $category_id = $GLOBALS['RENTALS'] !== $categories[0]->cat_ID ? $categories[0]->cat_ID : $categories[1]->cat_ID;
+    }else{
+        $category_id = $GLOBALS['RENTALS'];
+    }
+    
 
     $myposts = get_posts(array(
         'showposts' => -1, //add -1 if you want to show all posts
         'post_type' => 'post',
         'category' => $category_id,)
     );
-
+    $args = array(
+        'showposts' => -1, //add -1 if you want to show all posts
+        'post_type' => 'post',
+        'category' => $category_id,);
+    $query = new WP_Query( $args );
+    //echo $query[0]->ID;
     echo '<div class="display-category-wrappers">';
     foreach ($myposts as &$post){
         if($post->ID !== $excludeID){
