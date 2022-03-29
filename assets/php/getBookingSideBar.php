4b5dd94b6c39ab0174e956dcbd527d61fed79aa6
stellar-patrickpelayo <patrick.pelayo@stellarims.com>
2022-03-29 16:07:04 -0700

    uploading to site

--- assets/php/getBookingSideBar.php
+++ assets/php/getBookingSideBar.php
@@ -2,11 +2,14 @@
 
 function getBookingSideBar(){
     $gallery = do_shortcode("[slide-anything id='623']");
-    // echo "<div>
-    //     . $gallery . 
-    //     <div>
-    //         <a href='#'>BOOK NOW</a>
-    //     </div>
-    // </div>";
+    $img = get_template_directory_uri() . '/assets/images/lake.jpg';
+
+    echo "<div class='booking-side-bar'>
+        <img src='".$img."'>
+        <div>
+        test
+            <a href='#'>BOOK NOW</a>
+        </div>
+    </div>";
     echo $gallery;
 }
